fa8cc6a5b8e828dcbff8eae6a5bc69b852434973
Consti <constinic@gmail.com>
2022-04-12 13:52:22 -0300

    e

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const dis = require('discord.js');
 
 const client = new dis.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-client.login("process.env.BOT_TOKENhe");
+client.login("process.env.BOT_TOKEN");
 client.on('ready',()=> {
     console.log(`bot ready como ${client.user.tag}`);
 
