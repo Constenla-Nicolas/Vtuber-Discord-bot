34c6d27a4cbaf6bcf7acaa2aa266e1d293997abd
Consti <constinic@gmail.com>
2022-04-12 13:41:16 -0300

    p

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const dis = require('discord.js');
 
 const client = new dis.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-client.login("BOT_TOKEN");
+client.login("ODIxODgxNDg0MjgwNzI1NTE1.YFKKxw.56wT6F8ohfoBm3VSp4eJpS-fhjc");
 client.on('ready',()=> {
     console.log(`bot ready como ${client.user.tag}`);
 
